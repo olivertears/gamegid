eda3ac3cde4f34b238b404c3cef3e02e2891507e
Oliver Tears <olivertears@gmail.com>
2022-08-17 10:47:15 +0300

    fix: added prettier & destructured the component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
-import '../styles/globals.css'
-import React, {FC} from 'react';
-import {AppProps} from "next/app";
-import {wrapper} from "../store";
+import '../styles/globals.css';
+import React, { FC } from 'react';
+import { AppProps } from 'next/app';
+import { wrapper } from '../store';
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
-    <Component {...pageProps} />
-)
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-export default wrapper.withRedux(WrappedApp)
+export default wrapper.withRedux(WrappedApp);
